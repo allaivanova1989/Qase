532dc0d440cd736fd7bdb3ac7db1978091d1aa3c
allaivanova1989 <baby20086@rambler.ru>
2022-02-05 15:50:06 +0300

    change testListener

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -8,12 +8,18 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import utils.AllureUtils;
 
+import java.io.IOException;
+
 @Log4j2
 public class TestListener implements ITestListener {
-    @SneakyThrows
+
     @Override
     public void onTestFailure(ITestResult result) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
-        AllureUtils.takescreenshot();
+        try {
+            AllureUtils.takescreenshot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
