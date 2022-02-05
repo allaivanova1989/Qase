000aabe8b3c0e942526ab0ef645a432825a37afc
allaivanova1989 <baby20086@rambler.ru>
2022-02-05 18:19:12 +0300

    add api suite

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -12,13 +13,12 @@ import java.io.IOException;
 @Log4j2
 public class TestListener implements ITestListener {
 
+    @SneakyThrows
     @Override
     public void onTestFailure(ITestResult result) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
-        try {
+
             AllureUtils.takescreenshot();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
