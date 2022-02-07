6efcc160631819e03c625d42ac39fb3425e4428e
allaivanova1989 <baby20086@rambler.ru>
2022-02-07 08:50:59 +0300

    Add retry

--- src/test/java/tests/RunningTest.java
+++ src/test/java/tests/RunningTest.java
@@ -6,13 +6,16 @@ import modals.TestRunFactory;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.AllureUtils;
+
+import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.*;
 
 @Log4j2
 public class RunningTest extends BaseTest {
     @Test(description = "Creat and start new test run")
-    public void createNewTestRun() throws InterruptedException {
+    public void createNewTestRun() throws InterruptedException, IOException {
         log.info("Create test run");
                 open("/login");
                 loginPage.login(email,password);
@@ -30,6 +33,6 @@ public class RunningTest extends BaseTest {
         Thread.sleep(1000);
         Assert.assertTrue(isTestRunDetailsPage, "Страница Details не открылась.");
 
-
+        AllureUtils.takescreenshot();
     }
 }
--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -8,16 +8,21 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import utils.AllureUtils;
 
+import java.io.IOException;
 
 
 @Log4j2
 public class TestListener implements ITestListener {
 
-    @SneakyThrows
+
     @Override
     public void onTestFailure(ITestResult result) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+        try {
             AllureUtils.takescreenshot();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
